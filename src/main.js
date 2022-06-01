072d2aef188b68cdd1858c6739614f3f6626ce5a
RuiOkazaki <okazaki200226@gmail.com>
2022-06-02 01:15:47 +0900

    chore: 正しくdeployされるかの確認

--- src/main.js
+++ src/main.js
@@ -3,25 +3,21 @@ import contentsChange from './functions/contentsChange';
 import home from './problem/home';
 import dom from './problem/dom';
 
-window.addEventListener('DOMContentLoaded', () => {
-  const problems = {
-    home,
-    dom,
-  };
+const problems = {
+  home,
+  dom,
+};
 
-  console.log('test');
+const contentElement = document.getElementById('content');
 
-  const contentElement = document.getElementById('content');
-
-  window.addEventListener('load', () =>
-    contentsChange({ contentElement, problems })
-  );
-  document.querySelectorAll('a').forEach((a) =>
-    a.addEventListener('click', (event) => {
-      event.preventDefault();
-      const locationHref = event.target.href;
-      contentsChange({ contentElement, problems, locationHref });
-      window.history.replaceState('', '', locationHref);
-    })
-  );
-});
+window.addEventListener('load', () =>
+  contentsChange({ contentElement, problems })
+);
+document.querySelectorAll('a').forEach((a) =>
+  a.addEventListener('click', (event) => {
+    event.preventDefault();
+    const locationHref = event.target.href;
+    contentsChange({ contentElement, problems, locationHref });
+    window.history.replaceState('', '', locationHref);
+  })
+);
