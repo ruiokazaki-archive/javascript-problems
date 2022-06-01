3f24eab8637d138370f7143690b745a567d926be
RuiOkazaki <okazaki200226@gmail.com>
2022-06-02 00:38:37 +0900

    feat: urlによってページ切り替えするようにした

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
 import './style.css';
+
+import contentsChange from './functions/contentsChange';
+
+import home from './problem/home';
+import dom from './problem/dom';
+
+const problems = {
+  home,
+  dom,
+};
+
+const contentElement = document.getElementById('content');
+
+window.addEventListener('load', () =>
+  contentsChange({ contentElement, problems })
+);
+document.querySelectorAll('a').forEach((a) =>
+  a.addEventListener('click', (event) => {
+    event.preventDefault();
+    const locationHref = event.target.href;
+    contentsChange({ contentElement, problems, locationHref });
+    window.history.replaceState('', '', locationHref);
+  })
+);
