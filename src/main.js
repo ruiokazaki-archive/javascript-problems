29fc5a29afab763255914081853b611caee9378b
RuiOkazaki <okazaki200226@gmail.com>
2022-06-02 01:14:09 +0900

    chore: 正しくjsが読み込めるかの確認

--- src/main.js
+++ src/main.js
@@ -3,21 +3,25 @@ import contentsChange from './functions/contentsChange';
 import home from './problem/home';
 import dom from './problem/dom';
 
-const problems = {
-  home,
-  dom,
-};
+window.addEventListener('DOMContentLoaded', () => {
+  const problems = {
+    home,
+    dom,
+  };
 
-const contentElement = document.getElementById('content');
+  console.log('test');
 
-window.addEventListener('load', () =>
-  contentsChange({ contentElement, problems })
-);
-document.querySelectorAll('a').forEach((a) =>
-  a.addEventListener('click', (event) => {
-    event.preventDefault();
-    const locationHref = event.target.href;
-    contentsChange({ contentElement, problems, locationHref });
-    window.history.replaceState('', '', locationHref);
-  })
-);
+  const contentElement = document.getElementById('content');
+
+  window.addEventListener('load', () =>
+    contentsChange({ contentElement, problems })
+  );
+  document.querySelectorAll('a').forEach((a) =>
+    a.addEventListener('click', (event) => {
+      event.preventDefault();
+      const locationHref = event.target.href;
+      contentsChange({ contentElement, problems, locationHref });
+      window.history.replaceState('', '', locationHref);
+    })
+  );
+});
