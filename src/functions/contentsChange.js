fcab0d57edb89e216c2b62c576055ee471fa8b9f
RuiOkazaki <okazaki200226@gmail.com>
2022-06-02 20:46:31 +0900

    feat: homeのコンテンツを変数にした

--- src/functions/contentsChange.js
+++ src/functions/contentsChange.js
@@ -1,9 +1,12 @@
+const home =
+  '<h2>How to use</h2><p>navからカテゴリを選び問題を表示させます</p><h2>How to run locally</h2><ol><li>ターミナルで<code>git clone https://github.com/OkazakiRui/sandbox-js.git</code>を実行します。</li><li><code>npm i</code>で<code>node_modules</code>をインストールします。</li><li><code>npm run dev</code>で<a href="http://localhost:3000">localhost:3000</a>にてサーバーを立ち上げます。</li></ol>';
+
 const contentsChange = ({ contentElement, problems, locationHref }) => {
   const currentLocation = (locationHref || window.location.href)
     ?.split('#')[1]
     ?.split('-');
 
-  if (!currentLocation) return (contentElement.innerHTML = problems.home);
+  if (!currentLocation) return (contentElement.innerHTML = home);
 
   const [category, title] = currentLocation;
   contentElement.innerHTML =
