9ce481d113b99fde185e9185b217cf26a07f2a29
RuiOkazaki <okazaki200226@gmail.com>
2022-06-02 21:52:42 +0900

    feat: exclude_negative_numbers

---
+++ src/problem/array/exclude_negative_numbers.js
@@ -0,0 +1,26 @@
+const exclude_negative_numbers = `
+<h2>exclude_negative_numbers</h2>
+
+<h3>問題文</h3>
+<p>配列が与えられ、負の値を除く関数を作成せよ</p>
+
+<h3>制約</h3>
+<p>関数の引数には配列を受け取り、返り値に配列を返すこととする</p>
+<pre>
+@param {number[]}
+@return {number[]}
+</pre>
+
+<h3>入出力例</h3>
+<pre>
+<code>
+[1, 2, -3] // input
+[1, 2] // output
+
+[1, 2, -3, 4, -5, 10] // input
+[1, 2, 4, 10] // output
+</code>
+</pre>
+`;
+
+export default exclude_negative_numbers;
