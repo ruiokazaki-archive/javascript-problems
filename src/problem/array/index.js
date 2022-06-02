58040295411c42c500e14213fa901142422268af
RuiOkazaki <okazaki200226@gmail.com>
2022-06-02 21:22:07 +0900

    feat: 積が最大である3つの数値を見つけて、最大の積を出力する関数を作成せよ。

---
+++ src/problem/array/index.js
@@ -0,0 +1,8 @@
+import plus_one from './plus_one';
+import maximum_product_of_three_numbers from './maximum_product_of_three_numbers';
+
+const array = {
+  plus_one,
+  maximum_product_of_three_numbers,
+};
+export default array;
---
+++ src/problem/array/maximum_product_of_three_numbers.js
@@ -0,0 +1,34 @@
+const maximum_product_of_three_numbers = `
+<h2>maximum_product_of_three_numbers</h2>
+
+<h3>問題文</h3>
+<p>積が最大である3つの数値を見つけて、最大の積を出力する関数を作成せよ。</p>
+
+<h3>制約</h3>
+<p>関数の引数には配列を受け取り、返り値に値を返すこととする</p>
+<pre>
+@param {number[]}
+@return {number}
+</pre>
+
+<h3>入出力例</h3>
+<pre>
+<code>
+[1, 2, 3] // input
+6 // output
+
+[1, 2, 3, 4, 5, 10] // input
+200 // output
+</code>
+</pre>
+`;
+
+const ans = (nums) => {
+  nums.sort((a, b) => a - b);
+  const n = nums.length;
+  const a = nums[0] * nums[1] * nums[n - 1];
+  const b = nums[n - 1] * nums[n - 2] * nums[n - 3];
+  return Math.max(a, b);
+};
+
+export default maximum_product_of_three_numbers;
