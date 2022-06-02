5313b9f72d6cc63cc438d714b66b6df8114d5238
RuiOkazaki <okazaki200226@gmail.com>
2022-06-02 22:28:03 +0900

    feat: random integer

--- src/problem/dom/index.js
+++ src/problem/dom/index.js
@@ -1,8 +1,10 @@
 import background_color_change from './background_color_change';
 import stopwatch from './stopwatch';
+import random_integer from './random_integer.';
 
 const dom = {
   background_color_change,
   stopwatch,
+  random_integer,
 };
 export default dom;
---
+++ src/problem/dom/random_integer.js
@@ -0,0 +1,25 @@
+const random_integer = `
+<h2>random_integer</h2>
+
+<h3>問題文</h3>
+<p>ボタンを押すと画面にランダムな数字を表示するボタンを作成しなさい</p>
+
+<h3>制約</h3>
+<p>表示させる数字は整数にすること（切り上げ）</p>
+<p>作成した関数は<code>ボタン.addEventListener("click", 関数)</code>で動作するようにしなさい。</p>
+<p>関数の引数には数字を表示させたい要素を渡すこととする</p>
+<pre>
+@param {HTMLElement}
+@return {void}
+</pre>
+
+<h3>入出力例</h3>
+<pre>
+<code>
+document.getElementById("display") // param
+// return
+</code>
+</pre>
+`;
+
+export default random_integer;
