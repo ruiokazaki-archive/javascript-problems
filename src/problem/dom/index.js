de1ea0651dea14081dfbf855d1902623538f3b62
RuiOkazaki <okazaki200226@gmail.com>
2022-06-02 22:24:14 +0900

    feat: stopwatch

--- src/problem/dom/index.js
+++ src/problem/dom/index.js
@@ -1,6 +1,8 @@
 import background_color_change from './background_color_change';
+import stopwatch from './stopwatch';
 
 const dom = {
   background_color_change,
+  stopwatch,
 };
 export default dom;
---
+++ src/problem/dom/stopwatch.js
@@ -0,0 +1,24 @@
+const stapwatch = `
+<h2>stapwatch</h2>
+
+<h3>問題文</h3>
+<p>ボタンを押すとタイマーが進み、もう一度押すとタイマーが止まるストップウォッチを作成してください</p>
+
+<h3>制約</h3>
+<p>作成した関数は<code>ボタン.addEventListener("click", 関数)</code>で動作するようにしなさい。</p>
+<p>関数の引数には時間を表示させたい要素を渡すこととする</p>
+<pre>
+@param {HTMLElement}
+@return {void}
+</pre>
+
+<h3>入出力例</h3>
+<pre>
+<code>
+document.getElementById("display") // param
+// return
+</code>
+</pre>
+`;
+
+export default stapwatch</h2>;
