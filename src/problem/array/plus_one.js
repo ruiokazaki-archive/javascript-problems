3aba9688332702f061a0fee227b06178f9888b82
RuiOkazaki <okazaki200226@gmail.com>
2022-06-02 21:21:57 +0900

    feat: 配列が与えられ、その末尾に1を追加する関数を作成せよ。

---
+++ src/problem/array/plus_one.js
@@ -0,0 +1,26 @@
+const plus_one = `
+<h2>plus_one</h2>
+
+<h3>問題文</h3>
+<p>配列が与えられ、その末尾に1を追加する関数を作成せよ。</p>
+
+<h3>制約</h3>
+<p>関数の引数には配列を受け取り、返り値に配列を返すこととする</p>
+<pre>
+@param {number[]}
+@return {number[]}
+</pre>
+
+<h3>入出力例</h3>
+<pre>
+<code>
+[1, 2, 3] // input
+[1, 2, 4] // output
+
+[1, 2, 3, 4, 5, 10] // input
+[1, 2, 3, 4, 5, 11] // output
+</code>
+</pre>
+`;
+
+export default plus_one;
