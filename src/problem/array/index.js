f4ea35a7d0c831ecc8c79a16019fd9a23fd74b83
RuiOkazaki <okazaki200226@gmail.com>
2022-06-02 21:54:15 +0900

    feat: 読み込んだ

--- src/problem/array/index.js
+++ src/problem/array/index.js
@@ -1,8 +1,12 @@
 import plus_one from './plus_one';
 import maximum_product_of_three_numbers from './maximum_product_of_three_numbers';
+import reverse_array from './reverse_array';
+import exclude_negative_numbers from './exclude_negative_numbers';
 
 const array = {
   plus_one,
   maximum_product_of_three_numbers,
+  reverse_array,
+  exclude_negative_numbers,
 };
 export default array;
